0e1fd48f6172800dfa87619273da6f2c5b61aac5
zemira kehati <kehati10@gmail.com>
2023-02-06 00:53:32 +0200

    added styles component

---
+++ src/styles.js
@@ -0,0 +1,50 @@
+const rowStyle = {
+  display: "flex",
+};
+
+const boardStyle = {
+  backgroundColor: "#eee",
+  width: "208px",
+  alignItems: "center",
+  justifyContent: "center",
+  display: "flex",
+  flexDirection: "column",
+  border: "3px #eee solid",
+};
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+};
+
+const instructionsStyle = {
+  marginTop: "5px",
+  marginBottom: "5px",
+  fontWeight: "bold",
+  fontSize: "16px",
+};
+
+const buttonStyle = {
+  marginTop: "15px",
+  marginBottom: "16px",
+  width: "80px",
+  height: "40px",
+  backgroundColor: "#8acaca",
+  color: "white",
+  fontSize: "16px",
+};
+
+const squareStyle = {
+    width: "60px",
+    height: "60px",
+    backgroundColor: "#ddd",
+    margin: "4px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "20px",
+    color: "white",
+  };
+
+export {rowStyle, boardStyle, containerStyle, instructionsStyle, buttonStyle, squareStyle}
